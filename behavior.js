9b66fd56fdf19237bf4f0aed90a81f72ed945539
Jonathan M. Hammond <Pythonidaer@gmail.com>
2019-07-15 11:57:57 -0400

    HTML, CSS, and JS for website
    The Twitter links might not work because this time around I was using a local host. They should work fine on a remote server since I embedded them correctly through Twitter.
    
    Also, the FB/Twitter buttons probably won't share correctly, because they can't link to a local host. However, Just google the Facebook share button and Twitter share button plug in and it should direct you to change the script for the plug in, which matters due to URL connection.

---
+++ behavior.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+
+    thumbnailElement.addEventListener("click", function() {
+        if (thumbnailElement.className == "") {
+            thumbnailElement.className = "small"
+        }
+        else {
+            thumbnailElement.className = ""
+        };
+    });
+
+    var thumbnailElement2 = document.getElementById("smart_thumbnail2");
+
+    thumbnailElement2.addEventListener("click", function() {
+        if (thumbnailElement2.className == "") {
+            thumbnailElement2.className = "small"
+        }
+        else {
+            thumbnailElement2.className = ""
+        };
+    });
+
+
+
+    var thumbnailElement3 = document.getElementById("smart_thumbnail3");
+
+    thumbnailElement3.addEventListener("click", function() {
+        if (thumbnailElement3.className == "") {
+            thumbnailElement3.className = "small"
+        }
+        else {
+            thumbnailElement3.className = ""
+        };
+    });
+
+
+
+    var thumbnailElement4 = document.getElementById("smart_thumbnail4");
+
+    thumbnailElement4.addEventListener("click", function() {
+        if (thumbnailElement4.className == "") {
+            thumbnailElement4.className = "small"
+        }
+        else {
+            thumbnailElement4.className = ""
+        };
+    });
+
+    var thumbnailElement5 = document.getElementById("smart_thumbnail5");
+
+    thumbnailElement5.addEventListener("click", function() {
+        if (thumbnailElement5.className == "") {
+            thumbnailElement5.className = "small"
+        }
+        else {
+            thumbnailElement5.className = ""
+        };
+    });
+
+
+});
